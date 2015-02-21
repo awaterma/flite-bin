49cfeb4fa2eb7629edc5e485dced1a7a5ef54ba6
Andrew Waterman <awaterma@awaterma.net>
2015-02-21 16:43:06 -0600

    Updated path to point to flite binary.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path');
 var pkg = require('./package.json');
 
 module.exports = {
-    path : 'node_modules' + path.sep + pkg.name + 
-        path.sep + 'flite-1.4-release' + path.sep + 'bin'
+    path : '.' + path.sep + 'node_modules' + path.sep + pkg.name + 
+        path.sep + 'flite-1.4-release' + path.sep + 'bin' + path.sep +
+        'flite '
 }
